940faea9c3bc05d91bfbca8dd6cc298efc3126b7
SumraizAhmad <sumraizrandhawa@gmail.com>
2023-02-15 12:56:00 +0500

    email and password authentication with firebase in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import app from "./firebase";
+import { useState } from "react";
 
 function App() {
+  const auth = getAuth(app);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signup = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        alert("successfully created");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode);
+        // ..
+      });
+  };
+
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        alert("this user has signIn")
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        // const errorMessage = error.message;
+        console.log(errorCode);
+        alert("this user has not signIn")
+
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type={"email"}
+        placeholder="please enter your email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type={"password"}
+        placeholder="please enter your password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signup}>create account</button>
+      <button onClick={signIn}>Sign in</button>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBp3zb6q51_psoxT6CIWCI4-EfrZ3An-fY",
+  authDomain: "e-mail-auth-yt-d397f.firebaseapp.com",
+  projectId: "e-mail-auth-yt-d397f",
+  storageBucket: "e-mail-auth-yt-d397f.appspot.com",
+  messagingSenderId: "391927059251",
+  appId: "1:391927059251:web:519ba2277fb1da46ea6446"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
